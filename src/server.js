87147ef33ffabe68f271d10313b06e72f27e5375
mehtab shaikh <shaikhmehtab2501@gmail.com>
2026-03-28 21:10:36 +0530

    feat: added the docker config

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (_, res) => {
+  res.json({
+    message : "hello from docker app",
+    timestamp : new Date().toISOString(),
+    status : "Running successfully"
+  })
+}); 
+
+app.get('/health', (_, res) => {
+  res.json({
+    status : "OK",
+    timestamp : new Date().toISOString()
+  })
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
